dbcad583addd1f371f08d04da543b844e1679957
Joan Hérisson <joan.herisson@univ-evry.fr>
2021-10-15 15:31:55 +0200

    chore: refactor for conda

--- setup.py
+++ setup.py
@@ -5,30 +5,63 @@ All rights reserved.
 
 @author:  neilswainston
 '''
-# pylint: disable=exec-used
-import setuptools
+from setuptools import setup
+from os import path as os_path
 
 
-with open('README.md', 'r') as fle:
-    _LONG_DESCRIPTION = fle.read()
+## INFOS ##
+package     = 'liv_utils'
+descr       = 'liv-utils'
+url         = 'https://github.com/neilswainston/liv-utils'
+authors     = 'Neil Swainston'
+corr_author = 'neil.swainston@liverpool.ac.uk'
 
-with open('requirements.txt') as fle:
-    _REQUIREMENTS = fle.read().splitlines()
+## LONG DESCRIPTION
+with open(
+    os_path.join(
+        os_path.dirname(os_path.realpath(__file__)),
+        'README.md'
+    ),
+    'r',
+    encoding='utf-8'
+) as f:
+    long_description = f.read()
 
-setuptools.setup(
-    name='liv_utils-utils',
-    version='1.0',
-    author='Neil Swainston',
-    author_email='neil.swainston@liverpool.ac.uk',
-    description='liv-utils',
-    long_description=_LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    url='https://github.com/neilswainston/liv-utils',
-    packages=setuptools.find_packages(),
-    classifiers=[
+def get_version():
+    with open(
+        os_path.join(
+            os_path.dirname(os_path.realpath(__file__)),
+            'CHANGELOG.md'
+        ),
+        'r'
+    ) as f:
+        lines = f.readlines()
+    for line in lines:
+        if line.startswith('##'):
+            from re import search
+            m = search("\[(.+)\]", line)
+            if m:
+                return m.group(1)
+
+setup(
+    name                          = package,
+    version                       = get_version(),
+    author                        = authors,
+    author_email                  = corr_author,
+    description                   = descr,
+    long_description              = long_description,
+    long_description_content_type = 'text/markdown',
+    url                           = url,
+    packages                      = [package],
+    package_dir                   = {package: package},
+    include_package_data          = True,
+    test_suite                    = 'pytest',
+    license                       = 'MIT',
+    classifiers                   = [
         'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',
-    install_requires=_REQUIREMENTS
+    python_requires               = '>=3.7',
 )
+
