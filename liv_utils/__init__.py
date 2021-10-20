cf1372d64bbd57b43c9dcf45acad47dd96a409c0
Joan Hérisson <joan.herisson@univ-evry.fr>
2021-10-20 11:10:05 +0200

    trigger

--- liv_utils/__init__.py
+++ liv_utils/__init__.py
@@ -6,7 +6,6 @@ All rights reserved.
 @author:  neilswainston
 '''
 
-
 def pairwise(iterable):
     '''s -> (s0,s1), (s1,s2), (s2, s3), ...'''
     return [(iterable[i], iterable[i + 1]) for i in range(len(iterable) - 1)]
